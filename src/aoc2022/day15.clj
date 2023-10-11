8da9aeba77e6515106d1f8e68c77d04f5deb734a
Vitaly Samigullin <vrs@pilosus.org>
2023-10-11 17:18:19 +0200

    Day 15 - Part 1

--- src/aoc2022/day15.clj
+++ src/aoc2022/day15.clj
@@ -12,29 +12,49 @@
     (+ (abs (- r1 r2))
        (abs (- c1 c2)))))
 
-(defn pos-within-distance
-  "Given a starting position and a distance, generate a set of all
-  possible positions with the same distance of less"
-  [pos d]
+;; TODO for large distances ranges for columns may be huge. Keeping
+;; them in a set may be ineffective! Given that the range is
+;; continious monotonic sequence of integers, we may want to keep
+;; only [from, to). Then the task will boil down to:
+;; - finding an overlap of all the ranges
+;; - removing column numbers of given sensors and beacons from the overlap
+(defn cols-within-distance
+  "Return a set of columns of positions for a given row that are within
+  the given Manhattan distance of the given position"
+  [pos d row]
   (let [[r c] pos
-        rows (range (- r d) (+ r d 1))
-        cols (range (- c d) (+ c d 1))]
-    (into (set [])
-          (for [r' rows
-                c' cols
-                :let [pos' [r' c']
-                      d' (distance pos pos')]
-                :when (<= d' d)]
-            pos'))))
+        row-out-of-range? (or (< row (- r d)) (> row (+ r d)))]
+    (if row-out-of-range?
+      (set [])
+      (reduce
+       (fn [init c']
+         (let [pos' [row c']
+               d' (distance pos pos')]
+           (if (<= d' d)
+             (conj init c')
+             init)))
+       (set [])
+       (range (- c d) (+ c d 1))))))
+
+(defn occupied-cols
+  [pos row]
+  (let [[r c] pos]
+    (if (= r row)
+      #{c}
+      #{})))
 
 (defn coverage
   "Return a set of positions where no other beacon can be found for the
   given sensor, i.e. set of positions where Manhattance distance
   between a position and a sensor is less or equal to distance between
   the sensor and the beacon."
-  [sensor beacon]
-  (let [d (distance sensor beacon)]
-    (pos-within-distance sensor d)))
+  [sensor beacon row]
+  (let [d (distance sensor beacon)
+        cols (cols-within-distance sensor d row)]
+    (s/difference
+     cols
+     (occupied-cols sensor row)
+     (occupied-cols beacon row))))
 
 (def pos-regex #"(?s).*x=(?<sc>-?\d+), y=(?<sr>-?\d+):.*x=(?<bc>-?\d+), y=(?<br>-?\d+)")
 
@@ -75,69 +95,53 @@
   "Given a sequence of [sensor beacon] positions, return a set of all
   coverages for a given row"
   [pos-seq row]
-  (let [full (reduce
-              (fn [init e]
-                (let [[sensor beacon] e
-                      full-cov (coverage sensor beacon)
-                      row-cov (row-coverage full-cov row)]
-                  (s/union init row-cov)))
-              (set [])
-              pos-seq)
-        occ (occupied pos-seq)]
-    (s/difference full occ)))
-
-(defn borders
-  "Given a seq of all sensor/beacon positions, find max and min rows and cols"
-  [pos-seq]
-  (let [flat-pos (occupied pos-seq)
-        rows (sort-by first flat-pos)
-        cols (sort-by second flat-pos)]
-    {:row-min (-> rows first first)
-     :row-max (-> rows last first)
-     :col-min (-> cols first second)
-     :col-max (-> cols last second)}))
+  (reduce
+   (fn [init e]
+     (let [[sensor beacon] e
+           cov (coverage sensor beacon row)]
+       (s/union init cov)))
+   (set [])
+   pos-seq))
 
 ;; test data
 
-(def tl
-  ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
-   "Sensor at x=9, y=16: closest beacon is at x=10, y=16"
-   "Sensor at x=13, y=2: closest beacon is at x=15, y=3"
-   "Sensor at x=12, y=14: closest beacon is at x=10, y=16"
-   "Sensor at x=10, y=20: closest beacon is at x=10, y=16"
-   "Sensor at x=14, y=17: closest beacon is at x=10, y=16"
-   "Sensor at x=8, y=7: closest beacon is at x=2, y=10"
-   "Sensor at x=2, y=0: closest beacon is at x=2, y=10"
-   "Sensor at x=0, y=11: closest beacon is at x=2, y=10"
-   "Sensor at x=20, y=14: closest beacon is at x=25, y=17"
-   "Sensor at x=17, y=20: closest beacon is at x=21, y=22"
-   "Sensor at x=16, y=7: closest beacon is at x=15, y=3"
-   "Sensor at x=14, y=3: closest beacon is at x=15, y=3"
-   "Sensor at x=20, y=1: closest beacon is at x=15, y=3"])
-
 (comment
+  (def tl
+    ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+     "Sensor at x=9, y=16: closest beacon is at x=10, y=16"
+     "Sensor at x=13, y=2: closest beacon is at x=15, y=3"
+     "Sensor at x=12, y=14: closest beacon is at x=10, y=16"
+     "Sensor at x=10, y=20: closest beacon is at x=10, y=16"
+     "Sensor at x=14, y=17: closest beacon is at x=10, y=16"
+     "Sensor at x=8, y=7: closest beacon is at x=2, y=10"
+     "Sensor at x=2, y=0: closest beacon is at x=2, y=10"
+     "Sensor at x=0, y=11: closest beacon is at x=2, y=10"
+     "Sensor at x=20, y=14: closest beacon is at x=25, y=17"
+     "Sensor at x=17, y=20: closest beacon is at x=21, y=22"
+     "Sensor at x=16, y=7: closest beacon is at x=15, y=3"
+     "Sensor at x=14, y=3: closest beacon is at x=15, y=3"
+     "Sensor at x=20, y=1: closest beacon is at x=15, y=3"])
+
   (def tp (->> tl (map line->pos)))
   (def tr 10)
   (def tc (pos->coverage tp tr))
   ;; 26 positions
-  (count tc))
+  (count tc)
 
-(defn positions-without-beacons
-  [lines row]
-  (let [pos (map line->pos lines)
-        cov (pos->coverage pos row)]
-    cov))
+  ;; check perf
+  (time (cols-within-distance [2000469 3999724] 562976 2000000))
+  (time (cols-within-distance [8733 3995530] 1375195 2000000)))
 
 (defn count-positions
+  "Return number of pos in a given row where no beacons can be found"
   []
   (let [lines (-> (tools/input-path)
                   tools/path->lines)
-        result (positions-without-beacons lines 2000000)]
-    (count result)))
-
-;; real data
+        pos (map line->pos lines)
+        row 2000000
+        cov (pos->coverage pos row)]
+    (count cov)))
 
 (comment
-  (def lines (-> (tools/input-path)
-                 tools/path->lines))
-  (def pos (map line->pos lines)))
+  ;; Part 1 - 5176944 - 8900 ms
+  (count-positions))
