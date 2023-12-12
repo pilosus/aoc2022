935a1a5a8569a0e505aab3d51581dad42f030549
Vitaly Samigullin <vrs@pilosus.org>
2023-12-12 15:27:30 +0100

    Skip day 16 for now

---
+++ src/aoc2022/day16.clj
@@ -0,0 +1,8 @@
+(ns aoc2022.day16
+  "https://adventofcode.com/2022/day/16"
+  (:require
+   [aoc2022.tools :as tools]))
+
+(comment
+  "https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm"
+  "https://en.wikipedia.org/wiki/Shortest_path_problem")
